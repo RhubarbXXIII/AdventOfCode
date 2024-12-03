e4b9a4aee9cb676ec741b29951c4acf2a6617e4a
Zach <zachary.philip.wilson@gmail.com>
2024-12-03 00:37:04 -0500

    simplify file structure

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import os.path
 
 def read_file(filename: str):
     calling_file_path_directories = inspect.stack()[1].filename.split(os.path.sep)
-    year = calling_file_path_directories[-3]
-    day = calling_file_path_directories[-2]
-    with open(f"../../input/{year}/{day}/{filename}", 'r') as file:
+    year = calling_file_path_directories[-2]
+    day = calling_file_path_directories[-1].split('.')[0]
+    with open(f"../input/{year}/{day}/{filename}", 'r') as file:
         for line in file.readlines():
             yield line
 
