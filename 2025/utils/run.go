3a9adbe25290a5211ac7266227a5b1914d28b8d1
Zach <zachary.philip.wilson@gmail.com>
2025-12-02 08:02:13 -0500

    add submission

--- 2025/utils/run.go
+++ 2025/utils/run.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -34,22 +35,32 @@ func Run(
 
 	input := fetchInput(scriptDirectory, year, day, sessionCookie)
 
-	result1 := part1(input)
-	result2 := part2(input)
-
 	fmt.Printf("Running Day %s:\n", strings.TrimLeft(day, "0"))
-	fmt.Printf("  Part 1 Answer: %s\n", result1)
-	fmt.Printf("  Part 2 Answer: %s\n", result2)
 
-	// // Submit answers if requested
-	// if submit {
-	// 	fmt.Println("Submitting answers...")
-	// 	SubmitAnswer(year, dayStr, 1, result1)
-	// 	SubmitAnswer(year, dayStr, 2, result2)
-	// }
+	result1 := part1(input)
+	fmt.Printf("  Part 1 Answer: %s\n", result1)
+	if submit && result1 != "" {
+		correct, response := submitAnswer(year, day, "1", result1, sessionCookie)
+		if correct {
+			fmt.Printf("    %s\n", response)
+		} else {
+			log.Printf("    %s\n", response)
+		}
+	}
+
+	result2 := part2(input)
+	fmt.Printf("  Part 2 Answer: %s\n", result2)
+	if submit && result2 != "" {
+		correct, response := submitAnswer(year, day, "2", result2, sessionCookie)
+		if correct {
+			fmt.Printf("    %s\n", response)
+		} else {
+			log.Printf("    %s\n", response)
+		}
+	}
 }
 
-// Parse day from caller file path.
+// Parse the day from the script directory.
 func parseDay(callingFileDirectory string) string {
 	dayDirectory := filepath.Base(callingFileDirectory)
 	if len(dayDirectory) < 3 || dayDirectory[:3] != "day" {
@@ -77,28 +88,28 @@ func fetchInput(scriptDirectory, year, day, sessionCookie string) string {
 		fmt.Printf("Failed to read from input file, retrieving input from website: %v\n", err)
 	}
 
-	url := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, strings.TrimLeft(day, "0"))
+	urlString := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, strings.TrimLeft(day, "0"))
 
-	request, err := http.NewRequest("GET", url, nil)
+	request, err := http.NewRequest("GET", urlString, nil)
 	if err != nil {
-		log.Fatalf("Failed to create request for input: %v", err)
+		log.Fatalf("Failed to create request for fetching input: %v", err)
 	}
 
 	request.AddCookie(&http.Cookie{Name: "session", Value: sessionCookie})
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		log.Fatalf("Failed to send request for input: %v", err)
+		log.Fatalf("Failed to send request for fetching input: %v", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		log.Fatalf("Unexpected status %d when fetching inputs: %s", response.StatusCode, response.Body)
+		log.Fatalf("Unexpected status %d when fetching input: %s", response.StatusCode, response.Body)
 	}
 
 	inputBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Fatalf("Failed to read response body for input: %v", err)
+		log.Fatalf("Failed to read response body for fetching input: %v", err)
 	}
 
 	err = os.Mkdir(filepath.Dir(inputPath), 0644)
@@ -112,4 +123,54 @@ func fetchInput(scriptDirectory, year, day, sessionCookie string) string {
 	}
 
 	return string(inputBytes)
+}
+
+// Submit an answer to the website.
+func submitAnswer(year, day, part, answer, sessionCookie string) (bool, string) {
+	urlString := fmt.Sprintf("https://adventofcode.com/%s/day/%s/answer", year, strings.TrimLeft(day, "0"))
+
+	form := url.Values{}
+	form.Set("level", part)
+	form.Set("answer", answer)
+
+	request, err := http.NewRequest("POST", urlString, strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Fatalf("Failed to create request for submitting answer: %v", err)
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.AddCookie(&http.Cookie{Name: "session", Value: sessionCookie})
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Fatalf("Failed to send request for submitting answer: %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		log.Fatalf("Unexpected status %d when submitting answer: %s", response.StatusCode, response.Body)
+	}
+
+	resultBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body for submitting answer: %v", err)
+	}
+
+	result := strings.ToLower(string(resultBytes))
+
+	if strings.Contains(result, "that's the right answer") {
+		return true, "Submitted correct answer!"
+	} else if strings.Contains(result, "too low") {
+		return false, "Submitted answer was too low."
+	} else if strings.Contains(result, "too high") {
+		return false, "Submitted answer was too high."
+	} else if strings.Contains(result, "not the right answer") {
+		return false, "Submitted answer was incorrect."
+	} else if strings.Contains(result, "too recently") {
+		return false, "Submitted answer too recently."
+	} else if strings.Contains(result, "already got") || strings.Contains(result, "solving the right level") {
+		return true, "Already submitted correct answer."
+	} else {
+		return false, fmt.Sprintf("Unrecognized response message: %s", result)
+	}
 }
